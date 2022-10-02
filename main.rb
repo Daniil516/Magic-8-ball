cd9b1deac3c70a9bd944afab1799aae66a08bbcb
daniil516 <grishchenko_daniil_1@mail.ru>
2022-10-02 14:09:22 +0300

    code style fix

--- main.rb
+++ main.rb
@@ -1,40 +1,37 @@
-
 answers = [
-	# Positive answers
-	"It is certain.",
-	"It is decidedly so.",
-	"Without a doubt.",
-	"Yes definitely.",
-	"You may rely on it.",
+  # Positive answers
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes definitely.",
+  "You may rely on it.",
 
-	# Part positive answers
-	"As I see it, yes.",
-	"Most likely.",
-	"Outlook good.",
-	"Yes.",
-	"Signs point to yes. ",
+  # Part positive answers
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes. ",
 
-	# Neutral answers
-	"Reply hazy, try again.",
-	"Ask again later.",
-	"Better not tell you now.",
-	"Cannot predict now.",
-	"Concentrate and ask again. ",
+  # Neutral answers
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again. ",
 
-	# Negative answers
-	"Don't count on it.",
-	"My reply is no.",
-	"My sources say no.",
-	"Outlook not so good.",
-	"Very doubtful. "
+  # Negative answers
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful. "
 ]
 greetings = [
-	"Hello my dear friend. As for your question...",
-	"Who asks, he will receive an answer:",
-  	"Hello, mortal. Today is the answer for you:"
-
+  "Hello my dear friend. As for your question...",
+  "Who asks, he will receive an answer:",
+  "Hello, mortal. Today is the answer for you:"
 ]
 puts greetings.sample
-puts
 sleep(3)
 puts answers.sample
